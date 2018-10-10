94ea291c6dc6957c6a943453643db9c830c76cc3
GuilhermeBialas <guilhermebialas31@gmail.com>
2018-10-10 10:44:56 -0300

    att

---
+++ ExemploHibernate00/src/dao/AlunoDAO.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import bean.Aluno;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Guilherme Henrique Bialas
+ */
+public class AlunoDAO {
+
+    public List<Aluno> obterTodos() {
+        List<Aluno> alunos = new ArrayList<>();
+        return alunos;
+    }
+
+    public Aluno obterPeloId(int id) {
+
+        Aluno aluno = null;
+        return aluno;
+    }
+
+    public int inserir(Aluno aluno) {
+        return -1;
+    }
+
+    public boolean alterar(Aluno aluno) {
+        return false;
+    }
+
+    public boolean excluir(int id) {
+        return false;
+    }
+}
