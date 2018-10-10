69a17fc6bef22ce4853e05f7729d253cdb693d36
GuilhermeBialas <guilhermebialas31@gmail.com>
2018-10-10 10:56:00 -0300

    att

---
+++ ExemploHibernate00/src/database/estrutura.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS exemplo_hibernate_00;
+CREATE DATABASE exemplo_hibernate_00;
+USE exemplo_hibernate_00;
+
+
+
+CREATE TABLE sistemas(
+id INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(150)
+);
+
